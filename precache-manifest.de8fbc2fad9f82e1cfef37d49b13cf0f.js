2f35f0c9a5f0a555b07615fe47a238f47cef2b10
lucaskahl <lucaschangkahl@gmail.com>
2019-07-29 21:56:19 -0300

    Updates

---
+++ precache-manifest.de8fbc2fad9f82e1cfef37d49b13cf0f.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b29bc46eed9382303e25555c5903420",
+    "url": "/personal-portfolio-react/index.html"
+  },
+  {
+    "revision": "ccab6b71e8254e499759",
+    "url": "/personal-portfolio-react/static/js/2.5b7487a8.chunk.js"
+  },
+  {
+    "revision": "9e40ffbf18c0e2d50f1b",
+    "url": "/personal-portfolio-react/static/js/main.2df82a93.chunk.js"
+  },
+  {
+    "revision": "5624de7a6dd01c7855a8",
+    "url": "/personal-portfolio-react/static/js/runtime~main.db8e1b1d.js"
+  },
+  {
+    "revision": "6fcd34870474d2195788b231d4717f45",
+    "url": "/personal-portfolio-react/static/media/chip.6fcd3487.svg"
+  },
+  {
+    "revision": "225fbb805995f253c1aca0ac339e3c54",
+    "url": "/personal-portfolio-react/static/media/dot.225fbb80.svg"
+  },
+  {
+    "revision": "c5773f7c4190107e8c001200c01413b8",
+    "url": "/personal-portfolio-react/static/media/email.c5773f7c.svg"
+  },
+  {
+    "revision": "b53ff55e09a092f9f0cbbf74af01b715",
+    "url": "/personal-portfolio-react/static/media/github.b53ff55e.svg"
+  },
+  {
+    "revision": "f849dd3a502d0d251142bdea7e66aa67",
+    "url": "/personal-portfolio-react/static/media/header.f849dd3a.jpg"
+  },
+  {
+    "revision": "c01d16dbcb3a618e2cbfd41dd9d4a6df",
+    "url": "/personal-portfolio-react/static/media/linkedin.c01d16db.svg"
+  },
+  {
+    "revision": "34518e730d987df1cd219f2d122c7481",
+    "url": "/personal-portfolio-react/static/media/location.34518e73.svg"
+  },
+  {
+    "revision": "0ac9a5f0d245c3c60468b2a7b36b06ea",
+    "url": "/personal-portfolio-react/static/media/medium.0ac9a5f0.svg"
+  },
+  {
+    "revision": "46991746257d14776dd4a8767fb4395e",
+    "url": "/personal-portfolio-react/static/media/menu.46991746.svg"
+  },
+  {
+    "revision": "3b9c10e2a80418608c01032604517039",
+    "url": "/personal-portfolio-react/static/media/monitor.3b9c10e2.svg"
+  },
+  {
+    "revision": "0ff88b9bf06702e79d6c06981163075c",
+    "url": "/personal-portfolio-react/static/media/resume.0ff88b9b.svg"
+  }
+]);
